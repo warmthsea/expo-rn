3b5a58828f22acf53daff3c52c2f5dac15b20854
warmthsea <2586244885@qq.com>
2024-01-25 11:36:14 +0800

    add tailwindcss

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw.style('bg-red-100')}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={tw`text-red-500`}>
+        Hello World
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
